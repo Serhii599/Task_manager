6687216c19723b4926397bac8f278bc127260900
Serhii <Serhii@1111s-MacBook-Pro.local>
2025-12-05 16:30:02 +0200

    prod stuff added

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -10,4 +10,25 @@ STATIC_ROOT = "/static/"
 MEDIA_URL = "/media/"
 MEDIA_ROOT = "/media/"
 
-CSRF_TRUSTED_ORIGINS = ["*"]
+CSRF_TRUSTED_ORIGINS = ["*", "http://13.60.12.128", "https://13.60.12.128"]
+
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "handlers": {
+        "console": {
+            "class": "logging.StreamHandler",
+        },
+    },
+    "loggers": {
+        "django.request": {
+            "handlers": ["console"],
+            "level": "DEBUG",
+            "propagate": False,
+        },
+        "django": {
+            "handlers": ["console"],
+            "level": "DEBUG",
+        },
+    },
+}
