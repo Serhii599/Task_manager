5c563d6fe80c312e804b456da8736b75163626c3
Serhii <Serhii@1111s-MacBook-Pro.local>
2025-12-05 16:52:00 +0200

    prod stuff added

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -5,10 +5,10 @@ DEBUG = False
 ALLOWED_HOSTS = ["*", "13.60.12.128", "localhost"]
 
 STATIC_URL = "/static/"
-STATIC_ROOT = "/static/"
+STATIC_ROOT = "/static"
 
 MEDIA_URL = "/media/"
-MEDIA_ROOT = "/media/"
+MEDIA_ROOT = "/media"
 
 CSRF_TRUSTED_ORIGINS = ["http://13.60.12.128", "https://13.60.12.128"]
 
